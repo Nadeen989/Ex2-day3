e2ed814f4ac1e20bb835030bd877754479ecbd39
Nadeen989 <65255544+Nadeen989@users.noreply.github.com>
2022-08-02 19:35:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        //   System.out.println("Hello world!");
+
+        String users[] = new String[3];
+        users[0] = "Saleh";
+        users[1] = "Khalid";
+        users[2] = "Majed";
+
+        //    System.out.println(Arrays.toString(users));
+
+        {
+
+            for (int i = 0; i <= 33; i++) {
+                if (i % 5 == 0) {
+                    continue;
+
+                }
+        //        System.out.println(i);
+
+            }
+
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
